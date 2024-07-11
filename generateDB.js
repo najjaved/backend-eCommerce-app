4d718b3294cb4c8dae8b5ad41d3182a5b6ed05e9
Najma Javed <najma.javed@outlook.com>
2024-07-11 13:33:44 +0200

    updated data with image URLs for products

--- generateDB.js
+++ generateDB.js
@@ -1,8 +1,14 @@
 const fs = require('fs-extra'); // The fs module is a built-in Node.js module that provides an API for interacting with the file system
 const path = require('path'); // The path module is a built-in Node.js module that provides utilities for working with file and directory paths
 
-/* Data definition:
-JSON Server treats each top-level key as a database collection */
+
+/* Products images:
+categoriesBackground_URL = https://photos.app.goo.gl/snLGmkqqzHg2KVuY9
+herbalSupplements_URL = https://photos.app.goo.gl/4pfigJ6wX67KiPK99
+homemadeCosmetics_URL = https://photos.app.goo.gl/94AkQw4k5fNj2GTE8
+mineralSupplements_URL =https://photos.app.goo.gl/P2m64kBwLvjMXA7N8
+teas_URL = https://photos.app.goo.gl/huPu9F7UvQeMwpqUA 
+*/
 
 function generateDB() {
     const data = {
@@ -46,7 +52,9 @@ function generateDB() {
                 price: 9.99,
                 discount: 0.1, // 10% discount
                 stock: 100,
-                images: ["image1.jpg", "image2.jpg"]
+                images: ["https://photos.app.goo.gl/uwnckrZsijiXi3547", 
+                         "https://photos.app.goo.gl/VL2MJdjxMDuHmy5Z6" ]
+                
             },
             {
                 id: 2,
@@ -56,27 +64,29 @@ function generateDB() {
                 price: 15.99,
                 discount: 0.2, 
                 stock: 50,
-                images: ["image3.jpg", "image4.jpg"]
+                images: ["https://photos.app.goo.gl/8SynKEd9QY5TEziw6", 
+                         "https://photos.app.goo.gl/oxn8H44pS2EALDUm7"]
             },
             {
                 id: 3,
                 category: "Herbal Supplements",
                 name: "Moringa Capsules",
                 description: "Herbal supplement for overall health",
-                price: 25.99,
+                price: 22.99,
                 discount: 0.15, 
-                stock: 200,
-                images: ["image5.jpg", "image6.jpg"]
+                stock: 33,
+                images: ["https://photos.app.goo.gl/7aEYzZ12jbLH1zZu9"]
             },
             {
                 id: 4,
                 category: "Mineral Products",
-                name: "Shilajit Resin",
+                name: "Shilajit Golden Drops",
                 description: "Natural mineral supplement",
-                price: 39.99,
-                discount: 0.25, 
+                price: 59.99,
+                discount: 0.15, 
                 stock: 30,
-                images: ["image7.jpg", "image8.jpg"]
+                images: ["https://photos.app.goo.gl/arJnQZEEztz3iTEa9", 
+                         "https://photos.app.goo.gl/jyHTJEdvaJ6HCBsv5"]
             },
             {
                 id: 5,
@@ -86,17 +96,17 @@ function generateDB() {
                 price: 8.99,
                 discount: 0.1, 
                 stock: 80,
-                images: ["image9.jpg", "image10.jpg"]
+                images: ["https://photos.app.goo.gl/tzfXGDQVgU2nMsiX9"]
             },
             {
                 id: 6,
                 category: "Homemade Cosmetics",
                 name: "Aloe Vera Gel",
-                description: "Natural moisturizing gel",
-                price: 12.99,
+                description: "Natural moisturizing soothing alvera gel, perfect after use after sun",
+                price: 7.99,
                 discount: 0.05, // 5% discount
-                stock: 70,
-                images: ["image11.jpg", "image12.jpg"]
+                stock: 20,
+                images: ["https://photos.app.goo.gl/7HLLi72aqqoo8fY1A"]
             },
             {
                 id: 7,
@@ -106,27 +116,28 @@ function generateDB() {
                 price: 22.99,
                 discount: 0.1, 
                 stock: 150,
-                images: ["image13.jpg", "image14.jpg"]
+                images: ["https://photos.app.goo.gl/yKoArtMM3dd8F65cA", 
+                         "https://photos.app.goo.gl/ZXjDez2qrriLurs3A"]
             },
             {
                 id: 8,
                 category: "Mineral Products",
                 name: "Himalayan Salt",
-                description: "Natural mineral salt",
+                description: "Natural mineral salt from Khewra",
                 price: 5.99,
                 discount: 0.15, 
                 stock: 120,
-                images: ["image15.jpg", "image16.jpg"]
+                images: ["https://photos.app.goo.gl/iBvyj9JkVVm2v2iq9"]
             },
             {
                 id: 9,
                 category: "Herbal Teas",
                 name: "Matcha",
                 description: "Antioxidant-rich herbal tea",
-                price: 10.99,
-                discount: 0.1, 
-                stock: 90,
-                images: ["image17.jpg", "image18.jpg"]
+                price: 17.99,
+                discount: 0.3, 
+                stock: 40,
+                images: ["https://photos.app.goo.gl/W6CUDefwyc7gpVvy9"]
             },
             {
                 id: 10,
@@ -136,7 +147,7 @@ function generateDB() {
                 price: 14.99,
                 discount: 0.2, 
                 stock: 60,
-                images: ["image19.jpg", "image20.jpg"]
+                images: ["https://photos.app.goo.gl/Svu2AcLLfv2bvLi28"]
             },
             {
                 id: 11,
@@ -146,10 +157,7 @@ function generateDB() {
                 price: 9.99,
                 discount: 0.1,
                 stock: 100,
-                images: [
-                  "image1.jpg",
-                  "image2.jpg"
-                ]
+                images: ["https://photos.app.goo.gl/5qqHhvPfvUbNJxD57"]
               },
               {
                 id: 12,
@@ -159,11 +167,50 @@ function generateDB() {
                 price: 18.99,
                 discount: 0.1,
                 stock: 100,
-                images: [
-                  "image21.jpg",
-                  "image22.jpg"
-                ]
-              }
+                images: ["https://photos.app.goo.gl/8xmzzjjQJYN426eo7"]
+              },
+              {
+                id: 13,
+                category: "Homemade Cosmetics",
+                name: "Natural Soap",
+                description: "Homemade soap with organic materials",
+                price: 7.99,
+                discount: 0, 
+                stock: 22,
+                images: ["https://photos.app.goo.gl/tpsMP4iGPKHkWsLJ6"]
+            },
+            {
+                id: 14,
+                category: "Homemade Cosmetics",
+                name: "Almond Essential Oil",
+                description: "Homemade natural oil extracted from organic almonds",
+                price: 17.99,
+                discount: 0, 
+                stock: 35,
+                images: ["https://photos.app.goo.gl/cxCQgsToSpyXQvDP6"]
+            },
+            {
+                id: 15,
+                category: "Mineral Products",
+                name: "Shilajit Healing",
+                description: "Natural mineral supplement",
+                price: 39.99,
+                discount: 0.25, 
+                stock: 50,
+                images: ["https://photos.app.goo.gl/8Dc5Rjby8tTNBbU56",
+                         "https://photos.app.goo.gl/ccL49imQkwQg7xRZ9"]
+            },
+            {
+                id: 16,
+                category: "Mineral Products",
+                name: "Shilajit Golden",
+                description: "Natural mineral supplement",
+                price: 49.99,
+                discount: 0.15, 
+                stock: 30,
+                images: ["https://photos.app.goo.gl/Nw3417p5XNFy21EL9", 
+                         "https://photos.app.goo.gl/EXrNNLqfrQjG4pZq6"]
+            },
         ],
          //Collection of oders
         orders: [
